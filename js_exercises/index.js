5e1b7a6c03f5c98cf5066f917236b272dc545a37
nguyenleminhbao <nguyenleminhbaott5@gmail.com>
2023-06-16 08:30:38 +0700

    update js_exercises

--- js_exercises/index.js
+++ js_exercises/index.js
@@ -5,13 +5,21 @@ const listBtn= document.querySelectorAll(".caculator__keyboard-btn");
 listBtn.forEach((ele, index)=>{
     ele.addEventListener('click',()=>{
         let str= ele.innerHTML;
-        if(index!=14) string +=str.trim();
-        result.innerHTML=string;
-        console.log(string)
+        if(index!=14) 
+        {
+            string +=str.trim();
+            result.innerHTML=string;
+        }
+        else{
+            
+            let value = eval(string);
+            if (typeof(value) != Number) string = "0.0";
+            console.log(typeof(value),typeof(value) != Number)
+            result.innerHTML=value;
+            string= result.innerHTML;
+        }
+       
+
     })
 });
 
-document.querySelector('.btn-result').addEventListener('click',()=>{
-    result.innerHTML=eval(string);
-    string= result.innerHTML;
-})
