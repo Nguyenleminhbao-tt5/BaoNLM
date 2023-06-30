b83fab69e76079ed6391a3c6234813fea61846d0
nguyenleminhbao <nguyenleminhbaott5@gmail.com>
2023-06-30 11:37:08 +0700

    update-2 ValidateModel

--- migrate_exercises/HistoryMigration/app/models/user.rb
+++ migrate_exercises/HistoryMigration/app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
     validates :fulname, length: { in: 6..25 }
     validates :age, numericality: true
     validates :type, :sex, presence: true
+    validates :password, presence: true, length: { minimum: 8 }
+    validates :email, presence: true, uniqueness: true, format: { with: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, message: 'error format email'}
+      
 
 
     has_many :albums
