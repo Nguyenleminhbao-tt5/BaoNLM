9cb7f2613c57955fb42dcd0de1a2ef0c543b721f
nguyenleminhbao <nguyenleminhbaott5@gmail.com>
2023-07-11 09:07:37 +0700

    update config

--- migrate_exercises/HistoryMigration/config/application.rb
+++ migrate_exercises/HistoryMigration/config/application.rb
@@ -20,3 +20,4 @@ module Migrate
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
--- migrate_exercises/HistoryMigration/config/routes.rb
+++ migrate_exercises/HistoryMigration/config/routes.rb
@@ -1,6 +1,30 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+    get 'signup', to: 'devise/registrations#new'
+    get 'profile/edit', to: 'devise/registrations#edit'
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+
+  get 'profile/:id/tab_photos', to: 'profile#tab_photos', as: 'profile'
+  get 'profile/:id/tab_albums', to: 'profile#tab_albums', as: 'profile_albums'
+  get 'profile/:id/tab_followers', to: 'profile#tab_followers', as: 'profile_followers'
+  get 'profile/:id/tab_followings', to: 'profile#tab_followings', as: 'profile_followings'
+  get 'profile/edit', to: redirect('users/edit')
+
+  scope :user do
+    resources :photos, :albums, except: [:index, :show]
+  end
+
+  delete '/follows/:id', to: 'follows#destroy', as: 'follow'
+  post '/follows/:id', to: 'follows#create', as: 'follows'
+
+  get 'home/feed/:category', to: 'home#feed', as: 'home_feed'
+  get 'home/discover/:category', to: 'home#discover', as: 'home_discover'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#feed"
 end
