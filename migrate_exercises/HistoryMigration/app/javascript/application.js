4234d83e4009d8d2fbfb6504f41cd03dd223d9c3
nguyenleminhbao <nguyenleminhbaott5@gmail.com>
2023-07-11 09:15:19 +0700

    add file javascript

--- migrate_exercises/HistoryMigration/app/javascript/application.js
+++ migrate_exercises/HistoryMigration/app/javascript/application.js
@@ -1,3 +1,32 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+const listCard = document.querySelectorAll('.cardImg');
+const listTitle = document.querySelectorAll('.cardImg__right-heading')
+const listImg = document.querySelectorAll('.cardIMg__left-img');
+const listDescrip = document.querySelectorAll('.cardImg__right-text')
+
+listCard.forEach((ele,index)=>{
+    ele.addEventListener('click',()=>{
+        let title = listTitle[index].innerHTML;
+        let image = listImg[index].getAttribute('src')
+        let description = listDescrip[index].innerHTML
+
+        myModal.addEventListener('shown.bs.modal', () => {
+            console.log(listTitle[index].innerHTML)
+            console.log(listDescrip[index].innerHTML)
+            document.getElementById('exampleModalLabel').innerHTML= title
+            document.getElementById('modalBodyImg__img').setAttribute('src',image)
+            document.getElementById('modal_body__descrip').innerHTML = description 
+          })
+    
+    });
+})
+
+
