d4af8594a974e5c06fb2424eda4f264f94891788
nguyenleminhbao <nguyenleminhbaott5@gmail.com>
2023-07-11 09:03:48 +0700

    update-9HistoryMigration

--- migrate_exercises/HistoryMigration/db/schema.rb
+++ migrate_exercises/HistoryMigration/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_04_094027) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_040029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_094027) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.integer "mode", default: 0
+    t.string "list_image"
     t.index ["user_id"], name: "index_albums_on_user_id"
   end
 
@@ -65,6 +66,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_094027) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "fname"
+    t.string "lname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
