28d9b801df8a2f5c855ef0a55d9988d7bd9beab5
nguyenleminhbao <nguyenleminhbaott5@gmail.com>
2023-07-11 09:17:32 +0700

    update-1 Change Application Controller

--- migrate_exercises/HistoryMigration/app/controllers/application_controller.rb
+++ migrate_exercises/HistoryMigration/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+   
+    # {"controller"=>"devise/registrations", "action"=>"new"}
+    # {"controller"=>"devise/sessions", "action"=>"new"}
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :user_type, :age, :fname, :lname])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:fname, :lname, :email])
+    end
 end
