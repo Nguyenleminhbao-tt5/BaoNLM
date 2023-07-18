b85e4d8a3133269dfdfa44a7eb2880b0e9368d9b
nguyenleminhbao <nguyenleminhbaott5@gmail.com>
2023-07-18 09:37:07 +0700

    update-7 User Model

--- migrate_exercises/HistoryMigration/app/models/user.rb
+++ migrate_exercises/HistoryMigration/app/models/user.rb
@@ -6,26 +6,26 @@ class User < ApplicationRecord
 
     before_save :set_fullname
     
-    # enum mode: { guest: 0, client: 1, admin: 2}
+    enum user_type: { guest: 0, client: 1, admin: 2}
 
-    validates :fname, :lname ,length: { minimum: 2 }
+    validates :fname, :lname ,length: { minimum: 2, maximum: 25}
     validates :age, numericality: true
     validates :user_type, :sex, presence: true
     validates :password, presence: true, length: { minimum: 8 }
-    # validates :email, presence: true, uniqueness: true, format: { with: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, message: 'error format email'}
+    mount_uploader :avatar, ImageUploader
+    validates :email, presence: true, uniqueness: true, length: { maximum: 255}
       
 
-
-    has_many :albums
-    has_many :photos
+    has_many :albums , dependent: :destroy
+    has_many :photos , dependent: :destroy
 
     has_many :clients, class_name: "User",foreign_key: "manage_id"
     belongs_to :admin, class_name: "User", optional: true
 
-    has_many :follows_as_follower, foreign_key: :following_id, class_name: 'Follow'
+    has_many :follows_as_follower, foreign_key: :following_id, class_name: 'Follow' , dependent: :destroy
     has_many :followers, through: :follows_as_follower
 
-    has_many :follows_as_following, foreign_key: :follower_id, class_name: 'Follow'
+    has_many :follows_as_following, foreign_key: :follower_id, class_name: 'Follow'  , dependent: :destroy
     has_many :followings, through: :follows_as_following
     
 
